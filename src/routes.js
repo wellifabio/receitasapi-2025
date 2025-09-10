13f4fdb7c96bd409abb7b84980781b054ec58c45
Wellington Fábio de Oliveira Martins <44377232+wellifabio@users.noreply.github.com>
2025-09-10 16:57:42 -0300

    informações na rota raiz

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,49 @@ const router = express.Router();
 router.get('/', async (req, res) => {
     const rotas = {
         receitasapi: '/',
-        receitas: '/receitas',
+        rotas: [
+            {
+                rota:'/receitas',
+                verbo:'get',
+                resposta:'Lista todas as receitas'
+            },
+            {
+                rota:'/receitas/id',
+                verbo:'get',
+                resposta:'Lista apenas a receita pelo Id informado'
+            },
+            {
+                rota:'/receitas',
+                verbo:'post',
+                resposta:'Cadastra nova receita',
+                corpo:{
+                    nome:"Nome da receita",
+                    tipo:"Tipo [DOCE, SALGADA, BEBIDA]",
+                    ingredientes: "Ingredientes da receita",
+                    modoFazer: "Modo de fazer",
+                    img:"URL da imagem da receita",
+                    custoAproximado:"Tipo decimal"
+                }
+            },
+            {
+                rota:'/receitas/id',
+                verbo:'put',
+                resposta:'Altera a receita pelo id informado',
+                corpo:{
+                    nome:"Nome da receita",
+                    tipo:"Tipo [DOCE, SALGADA, BEBIDA]",
+                    ingredientes: "Ingredientes da receita",
+                    modoFazer: "Modo de fazer",
+                    img:"URL da imagem da receita",
+                    custoAproximado:"Tipo decimal"
+                }
+            },
+            {
+                rota:'/receitas/id',
+                verbo:'delete',
+                resposta:'Exclui a receita pelo id informado',
+            }
+        ]
     }
     res.json(rotas);
 });
